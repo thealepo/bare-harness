ad48add5ac7f4e4432c377779fb18e7bfb15052f
TheAlepo <sanchezc.alexe@gmail.com>
2026-09-14 11:23:48 -0400

    bare agent.py

---
+++ src/agent.py
@@ -0,0 +1,36 @@
+import json
+
+from llm import SYSTEM_PROMPT , call_llm
+from tools import TOOLS
+
+user_input = input('Enter your prompt> ')
+
+messages = [
+    {'role': 'system' , 'content': SYSTEM_PROMPT},
+    {'role': 'user' , 'content': user_input}
+]
+
+while True:
+    message = call_llm(user_input)
+    messages.append(message.model_dump(exclude_none=True))
+
+    if message.content:
+        print('\nAgent: ' , message.content , '\n')
+
+    # stop loop if no tools
+    if not message.tool_calls:
+        break
+
+    # invoke tool calls
+    for tool_call in message.tool_calls:
+        args = json.loads(tool_call.function.arguments)
+        result = TOOLS[tool_call.function.name](**args)
+        
+        print('Tool: ' , tool_call.function.name , args)
+        print(result , '\n')
+
+        messages.append({
+            'role': 'tool',
+            'tool_call_id': tool_call.id,
+            'content': result
+        })
