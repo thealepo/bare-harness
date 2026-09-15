0611682f57007caa9da286c58753fe76256462e5
TheAlepo <sanchezc.alexe@gmail.com>
2026-09-15 12:04:26 -0400

    extra functions

--- src/compact.py
+++ src/compact.py
@@ -6,3 +6,9 @@ SYSTEM_PROMPT = """."""  # TODO
 
 def is_compaction_needed(usage):
     return usage['prompt_token'] > 248_000 * 0.85  # hard-coded numbers. 248k context window, 85% full
+
+def summarize(messages):
+    pass
+
+def compact(messages):
+    pass
